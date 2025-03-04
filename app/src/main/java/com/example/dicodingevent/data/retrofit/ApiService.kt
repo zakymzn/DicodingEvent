002cf2725fa99c9351d52a4919f7c2e68c3f7952
zakymzn <mamurzakynurrokhman@gmail.com>
2025-03-04 16:07:33 +0700

    setup api service dan api config

--- app/src/main/java/com/example/dicodingevent/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/dicodingevent/data/retrofit/ApiService.kt
@@ -1,4 +1,27 @@
 package com.example.dicodingevent.data.retrofit
+import com.example.dicodingevent.data.response.EventDetailResponse
+import com.example.dicodingevent.data.response.EventListResponse
+import retrofit2.Call
+import retrofit2.http.*
 
-class ApiService {
+interface ApiService {
+    @GET("events?active={active}")
+    fun getEvents(
+        @Path("active") active: Int,
+    ): Call<EventListResponse>
+
+    @GET("events?q={query}")
+    fun searchEvents(
+        @Path("query") query: String,
+    ): Call<EventListResponse>
+
+    @GET("events?limit={limit}")
+    fun getEventsLimit(
+        @Path("limit") limit: Int,
+    ): Call<EventListResponse>
+
+    @GET("events/{id}")
+    fun getEventDetail(
+        @Path("id") id: String
+    ): Call<EventDetailResponse>
 }

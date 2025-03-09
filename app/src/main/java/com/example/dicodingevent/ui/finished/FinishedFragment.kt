2ea4b8adccc97813ba8c0271bc3445a3068f18dc
zakymzn <mamurzakynurrokhman@gmail.com>
2025-03-09 23:59:59 +0700

    membuat halaman finished events

--- app/src/main/java/com/example/dicodingevent/ui/finished/FinishedFragment.kt
+++ app/src/main/java/com/example/dicodingevent/ui/finished/FinishedFragment.kt
@@ -4,37 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dicodingevent.data.response.ListEventsItem
 import com.example.dicodingevent.databinding.FragmentFinishedBinding
+import com.example.dicodingevent.ui.FinishedEventViewModel
 
 class FinishedFragment : Fragment() {
 
-private var _binding: FragmentFinishedBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentFinishedBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val finishedViewModel =
-            ViewModelProvider(this).get(FinishedViewModel::class.java)
-
-    _binding = FragmentFinishedBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textFinished
-    finishedViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+    private fun getFinishedEventsData(eventsItem: List<ListEventsItem>) {
+        val adapter = FinishedEventAdapter()
+        adapter.submitList(eventsItem)
+        binding.rvFinishedEvents.adapter = adapter
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    private fun showLoading(isLoading: Boolean) {
+        binding.pbFinishedEvents.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val finishedEventViewModel = ViewModelProvider(this).get(FinishedEventViewModel::class.java)
+        _binding = FragmentFinishedBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        finishedEventViewModel.listEvents.observe(viewLifecycleOwner) { eventsItem ->
+            binding.rvFinishedEvents.layoutManager = LinearLayoutManager(requireActivity())
+            getFinishedEventsData(eventsItem)
+        }
+
+        finishedEventViewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }

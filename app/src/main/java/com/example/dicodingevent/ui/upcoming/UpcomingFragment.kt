96176f146899343544631d7abc67853249ec608e
zakymzn <mamurzakynurrokhman@gmail.com>
2025-03-09 23:41:08 +0700

    membuat halaman upcoming events

--- app/src/main/java/com/example/dicodingevent/ui/upcoming/UpcomingFragment.kt
+++ app/src/main/java/com/example/dicodingevent/ui/upcoming/UpcomingFragment.kt
@@ -4,37 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dicodingevent.data.response.ListEventsItem
 import com.example.dicodingevent.databinding.FragmentUpcomingBinding
+import com.example.dicodingevent.ui.UpcomingEventViewModel
 
 class UpcomingFragment : Fragment() {
 
-private var _binding: FragmentUpcomingBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentUpcomingBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val upcomingViewModel =
-            ViewModelProvider(this).get(UpcomingViewModel::class.java)
-
-    _binding = FragmentUpcomingBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textUpcoming
-    upcomingViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+    private fun getUpcomingEventsData(eventsItem: List<ListEventsItem>) {
+        val adapter = UpcomingEventAdapter()
+        adapter.submitList(eventsItem)
+        binding.rvUpcomingEvents.adapter = adapter
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    private fun showLoading(isLoading: Boolean) {
+        binding.pbUpcomingEvents.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val upcomingEventViewModel = ViewModelProvider(this).get(UpcomingEventViewModel::class.java)
+        _binding = FragmentUpcomingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        upcomingEventViewModel.listEvents.observe(viewLifecycleOwner) {eventsItem ->
+            binding.rvUpcomingEvents.layoutManager = LinearLayoutManager(requireActivity())
+            getUpcomingEventsData(eventsItem)
+        }
+
+        upcomingEventViewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }

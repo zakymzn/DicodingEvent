d46c8b27cdb50d10c1f0ae23b1d147e3b57d4c5d
zakymzn <mamurzakynurrokhman@gmail.com>
2025-03-21 02:19:45 +0700

    menambahkan fitur deteksi koneksi internet

--- app/src/main/java/com/example/dicodingevent/MainActivity.kt
+++ app/src/main/java/com/example/dicodingevent/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.dicodingevent
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,6 +19,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val notConnected = intent.getBooleanExtra(ConnectivityManager
+                .EXTRA_NO_CONNECTIVITY, false)
+            if (notConnected) {
+                disconnected()
+            } else {
+                connected()
+            }
+        }
+    }
+
+    private fun disconnected() {
+        binding.llNoInternetConnection.visibility = View.VISIBLE
+        binding.clActivityMain.visibility = View.GONE
+    }
+
+    private fun connected() {
+        binding.llNoInternetConnection.visibility = View.GONE
+        binding.clActivityMain.visibility = View.VISIBLE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,5 +51,17 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         navView.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        registerReceiver(broadcastReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        unregisterReceiver(broadcastReceiver)
     }
 }

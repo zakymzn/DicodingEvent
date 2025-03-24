d5b833c9a18952abc6a57a7efec802927c754ce5
zakymzn <mamurzakynurrokhman@gmail.com>
2025-03-24 16:52:35 +0700

    menambahkan kondisi untuk memunculkan bottom navigation ketika tombol fisik untuk kembali ditekan

--- app/src/main/java/com/example/dicodingevent/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/dicodingevent/ui/detail/DetailFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
@@ -114,6 +115,11 @@ class DetailFragment : Fragment() {
         val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)
 
         toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+            bottomNavigationView.animate()?.translationY(0f)?.setDuration(200)
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback {
             findNavController().navigateUp()
             bottomNavigationView.animate()?.translationY(0f)?.setDuration(200)
         }

fbf5d09663fbf6f221af22894043fc6cd09d49ed
zakymzn <mamurzakynurrokhman@gmail.com>
2025-04-14 21:33:47 +0700

    menambahkan query

--- app/src/main/java/com/example/dicodingevent/data/local/room/EventDao.kt
+++ app/src/main/java/com/example/dicodingevent/data/local/room/EventDao.kt
@@ -9,17 +9,26 @@ interface EventDao {
     @Query("SELECT * FROM event ORDER BY id DESC")
     fun getEvents(): LiveData<List<EventEntity>>
 
+    @Query("SELECT * FROM event WHERE id = :id")
+    fun getDetailEvent(id: Int?): LiveData<EventEntity>
+
     @Query("SELECT * FROM event WHERE favorited = 1")
     fun getFavoritedEvent(): LiveData<List<EventEntity>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertEvent(event: List<EventEntity>)
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertDetailEvent(event: EventEntity)
+
     @Update
     suspend fun updateEvent(event: EventEntity)
 
     @Query("DELETE FROM event WHERE favorited = 0")
     suspend fun deleteAll()
+
+    @Query("DELETE FROM event WHERE id = :id")
+    suspend fun deleteById(id: Int?)
 
     @Query("SELECT EXISTS(SELECT * FROM event WHERE id = :id AND favorited = 1)")
     suspend fun isEventFavorited(id: Int?): Boolean

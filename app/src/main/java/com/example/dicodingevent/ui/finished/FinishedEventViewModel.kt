f3c07372046c88d8e5f486362cf4cd74139cb0c5
zakymzn <mamurzakynurrokhman@gmail.com>
2025-04-14 21:35:24 +0700

    perapihan kode

--- app/src/main/java/com/example/dicodingevent/ui/finished/FinishedEventViewModel.kt
+++ app/src/main/java/com/example/dicodingevent/ui/finished/FinishedEventViewModel.kt
@@ -1,19 +1,10 @@
 package com.example.dicodingevent.ui.finished
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.dicodingevent.data.EventRepository
 import com.example.dicodingevent.data.local.entity.EventEntity
-import com.example.dicodingevent.data.remote.response.EventListResponse
-import com.example.dicodingevent.data.remote.response.ListEventsItem
-import com.example.dicodingevent.data.remote.retrofit.ApiConfig
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class FinishedEventViewModel(private val eventRepository: EventRepository) : ViewModel() {
 
@@ -32,47 +23,4 @@ class FinishedEventViewModel(private val eventRepository: EventRepository) : Vie
             eventRepository.setEventFavorite(event, false)
         }
     }
-
-//    private val _listEvents = MutableLiveData<List<ListEventsItem>>()
-//    val listEvents: LiveData<List<ListEventsItem>> = _listEvents
-//
-//    private val _isLoading = MutableLiveData<Boolean>()
-//    val isLoading: LiveData<Boolean> = _isLoading
-//
-//    private val _errorMessage = MutableLiveData<String>()
-//    val errorMessage: LiveData<String> = _errorMessage
-//
-//    init {
-//        getFinishedEvents()
-//    }
-//
-//    private fun getFinishedEvents() {
-//        _isLoading.value = true
-//        val client = ApiConfig.getApiService().getEvents(FINISHED_EVENT, 40)
-//        client.enqueue(object : Callback<EventListResponse> {
-//            override fun onResponse(
-//                call: Call<EventListResponse>,
-//                response: Response<EventListResponse>
-//            ) {
-//                _isLoading.value = false
-//                if (response.isSuccessful) {
-//                    _listEvents.value = response.body()?.listEvents
-//                } else {
-//                    _errorMessage.value = response.message()
-//                    Log.e(TAG, "onFailure: ${response.message()}")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<EventListResponse>, t: Throwable) {
-//                _isLoading.value = false
-//                _errorMessage.value = t.message.toString()
-//                Log.e(TAG, "onFailure: ${t.message.toString()}")
-//            }
-//        })
-//    }
-//
-//    companion object {
-//        private const val TAG = "FinishedEventViewModel"
-//        private const val FINISHED_EVENT = 0
-//    }
 }
--- app/src/main/java/com/example/dicodingevent/ui/finished/FinishedFragment.kt
+++ app/src/main/java/com/example/dicodingevent/ui/finished/FinishedFragment.kt
@@ -8,12 +8,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dicodingevent.R
 import com.example.dicodingevent.data.Result
-import com.example.dicodingevent.data.remote.response.ListEventsItem
 import com.example.dicodingevent.databinding.FragmentFinishedBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -22,27 +20,12 @@ class FinishedFragment : Fragment() {
     private var _binding: FragmentFinishedBinding? = null
     private val binding get() = _binding
 
-//    private fun getFinishedEventsData(eventsItem: List<ListEventsItem>) {
-//        val adapter = FinishedEventAdapter()
-//        adapter.submitList(eventsItem)
-//        binding.rvFinishedEvents.adapter = adapter
-//    }
-//
-//    private fun showLoading(isLoading: Boolean) {
-//        binding.pbFinishedEvents.visibility = if (isLoading) View.VISIBLE else View.GONE
-//    }
-//
-//    private fun showErrorMessage(errorMessage: String) {
-//        Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
-//    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFinishedBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
 
         return binding?.root
     }
@@ -88,26 +71,10 @@ class FinishedFragment : Fragment() {
             }
         }
 
-//        val finishedEventViewModel = ViewModelProvider(this)[FinishedEventViewModel::class.java]
-
         rvFinishedEvents?.apply {
             layoutManager = LinearLayoutManager(requireActivity())
             adapter = finishedEventAdapter
         }
-
-//
-//        finishedEventViewModel.listEvents.observe(viewLifecycleOwner) { eventsItem ->
-//            binding.rvFinishedEvents.layoutManager = LinearLayoutManager(requireActivity())
-//            getFinishedEventsData(eventsItem)
-//        }
-//
-//        finishedEventViewModel.isLoading.observe(viewLifecycleOwner) {
-//            showLoading(it)
-//        }
-//
-//        finishedEventViewModel.errorMessage.observe(viewLifecycleOwner) {
-//            showErrorMessage(it)
-//        }
 
         if (bottomNavigationView == null) {
             Log.e("FinishedFragment", "BottomNavigationView is null")

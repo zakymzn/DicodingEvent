38c97229d85a45bb2604d2d04057d28d6871cc86
zakymzn <mamurzakynurrokhman@gmail.com>
2025-04-14 21:35:53 +0700

    menerapkan hilt

--- app/src/main/java/com/example/dicodingevent/di/Injection.kt
+++ app/src/main/java/com/example/dicodingevent/di/Injection.kt
@@ -5,9 +5,19 @@ import com.example.dicodingevent.data.EventRepository
 import com.example.dicodingevent.data.local.room.EventDatabase
 import com.example.dicodingevent.data.remote.retrofit.ApiConfig
 import com.example.dicodingevent.utils.AppExecutors
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object Injection {
-    fun provideRepository(context: Context): EventRepository {
+    @Singleton
+    @Provides
+    fun provideRepository(@ApplicationContext context: Context): EventRepository {
         val apiService = ApiConfig.getApiService()
         val database = EventDatabase.getInstance(context)
         val dao = database.eventDao()
